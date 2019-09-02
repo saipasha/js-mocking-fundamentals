ff0a89a021688f0b26fe3e7dac951b0e5f5cefde
Isabella <isabellaonate@gmail.com>
2019-09-02 13:16:17 -0500

    comments on learning

--- src/no-framework/monkey-patching.js
+++ src/no-framework/monkey-patching.js
@@ -2,11 +2,15 @@ const assert = require('assert')
 const thumbWar = require('../thumb-war')
 const utils = require('../utils')
 
+// get original Winner
 const originalGetWinner = utils.getWinner
+
+// reassign winner to always match p1
 utils.getWinner = (p1, p2) => p1
 
+// run tests for that specific case
 const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
 assert.strictEqual(winner, 'Kent C. Dodds')
 
-// cleanup
+// cleanup and reassign original winner so other tests won't be affected by this
 utils.getWinner = originalGetWinner
--- src/utils.js
+++ src/utils.js
@@ -3,6 +3,7 @@
 // is making a call to some third party machine learning
 // service that has a testing environment we don't control
 // and is unreliable so we want to mock it out for tests.
+
 function getWinner(player1, player2) {
   const winningNumber = Math.random()
   return winningNumber < 1 / 3
